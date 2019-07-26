99c106a18140819e01f3a86a2f97c82c61074df7
yoshijulas <yoshijulas@live.com>
2019-07-26 06:02:42 -0500

    Added Colors

--- .config/polybar/scripts/whatsapp.sh
+++ .config/polybar/scripts/whatsapp.sh
@@ -3,13 +3,16 @@
 # sudo pacman -S wmctrl
 # You need to have open a page of WhatsApp elsewhere than your main daily window
 # This is my first script, if is bad, ID = yoshijulas
+COLOR_NOTIF='#44ff44'
+COLOR_CLOSED='#ff0000'
+
 
 com=$(wmctrl -l | grep 'WhatsApp' | awk '{print $4}')
 comnum=$(echo $com | sed 's/[()]//g')
 if [[ ! $(wmctrl -l | grep 'WhatsApp ') ]]; then
-  echo " Not Opened"; exit
+  echo "%{F$COLOR_CLOSED} Not Opened%{F-}"; exit
 elif [[ $com == "WhatsApp" ]]; then
   echo " 0 Notif"; exit
 else
-  echo " $comnum Notif"
+  echo "%{F$COLOR_NOTIF} $comnum Notif%{F-}"
 fi
