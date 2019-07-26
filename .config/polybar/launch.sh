7c3abf217a04ec5167ecbf22fd1f41bf23fc21db
yoshijulas <yoshijulas@live.com>
2019-07-26 06:03:59 -0500

    Added New Bars For Different Res Sizes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar Polybar &
-polybar PolybarBot &
+
+width=$(xrandr | grep current | sed 's/.*\(current\s\w*\s\w\s\w*\).*/\1/' | sed 's/\(current\)//g' | awk '{ print $1 }' )
+
+if [[ $width > "1600" ]]; then
+  polybar Polybar &
+  polybar PolybarBot &
+else
+  polybar PolybarSmall &
+  polybar PolybarBotSmall &
+fi
+
 echo "Polybar launched..."
