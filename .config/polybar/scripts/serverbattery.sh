9523e80d1a9aa77618ce56ed484121c2805f6a76
yoshijulas <yoshijulas@live.com>
2019-07-27 20:17:19 -0500

    Updated

--- .config/polybar/scripts/serverbattery.sh
+++ .config/polybar/scripts/serverbattery.sh
@@ -3,11 +3,15 @@
 serverdir='/mnt/server'
 batterydir='/sys/class/power_supply'
 
+if [[ ! $(ls $serverdir) ]]; then
+  exit
+fi
+
 batperc=$(tr -d '\0' <$serverdir$batterydir/BAT1/capacity)
 acstat=$(tr -d '\0' <$serverdir$batterydir/ADP1/online)
 
 if [[ $acstat == '1' ]]; then
-  if [[ $batperc > '79' ]]; then
+  if [[ $batperc > '75' ]]; then
     echo "Full"
   else
     echo "Chk" $batperc
@@ -15,5 +19,5 @@ if [[ $acstat == '1' ]]; then
 elif [[ $batperc < '45' ]]; then
   echo "%{F#ff0000}Bat $batperc%{F-}"
 else
-  echo "Bat $batperc"
+  echo "%{F#ffff00}Bat $batperc%{F-}"
 fi
