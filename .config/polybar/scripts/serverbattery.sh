1de4551c5c61eff1ab95c61397728a4d9e664105
yoshijulas <yoshijulas@live.com>
2019-08-21 00:46:23 -0500

    Fixed Some Command Errors

--- .config/polybar/scripts/serverbattery.sh
+++ .config/polybar/scripts/serverbattery.sh
@@ -3,7 +3,7 @@
 serverdir='/mnt/server'
 batterydir='/sys/class/power_supply'
 
-if [[ ! $(ls $serverdir) ]]; then
+if [[ ! -d $serverdir$batterydir ]]; then
   exit
 fi
 
@@ -11,8 +11,10 @@ batperc=$(tr -d '\0' <$serverdir$batterydir/BAT1/capacity)
 acstat=$(tr -d '\0' <$serverdir$batterydir/ADP1/online)
 
 if [[ $acstat == '1' ]]; then
-  if [[ $batperc > '75' ]]; then
+  if [[ $batperc > '70' ]]; then
     echo "Full"
+  elif [[ $batperc < '40' ]]; then
+    echo "%{F#ff0000}Chk $batperc%{F-}"
   else
     echo "Chk" $batperc
   fi
